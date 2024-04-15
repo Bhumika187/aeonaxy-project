3cc6378019eb014c153b799d90895ee728782ac4
Bhumika187 <pantbhumika5@gmail.com>
2024-04-15 22:30:20 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import SignUpPage from "./Components/SignUpPage/SignUpPage";
 import ProfileCreationPage from './Components/ProfilePage/ProfilePage';
-import {Routes,Route,Link} from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import ImageSelectionPage from './Components/ImageSelectionPage/ImageSelectionPage';
 import LastPage from './Components/LastPage/LastPage';
 
 function App(){
   return(
     <>
+    <BrowserRouter>
    <Routes>
      <Route path='/' element={<SignUpPage/>}></Route>
      <Route path='/profile' element={ <ProfileCreationPage/>}></Route>
      <Route path='/img' element={ <ImageSelectionPage/>}></Route>
      <Route path='/finish' element={<LastPage/>}></Route>
    </Routes>
+   </BrowserRouter>
    </>
   );
 }
